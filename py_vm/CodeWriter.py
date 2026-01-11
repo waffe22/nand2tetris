c2198e218091f8085d005c0f20941d125ccb8481
waffe22 <146151816+waffe22@users.noreply.github.com>
2026-01-11 19:37:05 +0900

    Add files via upload

--- py_vm/CodeWriter.py
+++ py_vm/CodeWriter.py
@@ -38,18 +38,29 @@ class CodeWriter():
             return f"@{index}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
         if segment=="static":
             if command=="push":
-                return f"@{self.filename+str(index)}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                return f"@{self.filename}_{str(index)}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
             if command=="pop":
-                return f"@SP\nAM=M-1\nD=M\n@{self.filename+str(index)}\nM=D\n"
+                return f"@SP\nAM=M-1\nD=M\n@{self.filename}_{str(index)}\nM=D\n"
         d={"argument":"ARG","local":"LCL","pointer":"3","this":"THIS","that":"THAT","temp":"5"}
         if segment in d:
             if command=="push":
-                return f"@{d[segment]}\nD=M\n@{index}\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                if segment in ["pointer","temp"]:
+                    return f"@{int(d[segment])+int(index)}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                if int(index)==0:
+                    return f"@{d[segment]}\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                else:
+                    return f"@{d[segment]}\nD=M\n@{index}\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
             if command=="pop":
                 if segment in ["pointer","temp"]:
                     return f"@SP\nAM=M-1\nD=M\n@{int(index)+int(d[segment])}\nM=D\n"
-
-                return f"@{d[segment]}\nD=M\n@{index}\nD=A+D\n@R15\nM=D\n@SP\nAM=M-1\nD=M\n@R15\nA=M\nM=D\n"
+                
+                if int(index)==0:
+                    return f"@SP\nAM=M-1\nD=M\n@{d[segment]}\nA=M\nM=D\n"
+                elif int(index)<7:
+                    inc="A=A+1\n"*int(index)
+                    return f"@SP\nAM=M-1\nD=M\n@{d[segment]}\nA=M\n{inc}\nM=D\n"
+                else:
+                    return f"@{d[segment]}\nD=M\n@{index}\nD=A+D\n@R15\nM=D\n@SP\nAM=M-1\nD=M\n@R15\nA=M\nM=D\n"
 
 
     def writeLabel(self,label):
@@ -62,10 +73,11 @@ class CodeWriter():
         return f"@SP\nAM=M-1\nD=M\n@{self.functionName}${label}\nD;JNE\n"
 
     def writeFunction(self,functionName,numLocals):
+        print(functionName,numLocals)
         self.functionName=functionName
         x=str(func_call_counts.setdefault("function",0))
         func_call_counts["function"]+=1
-        return f"({functionName})\n@RET_ADDRESS_FUNCTION{x}\nD=A\n@R14\nM=D\n@{numLocals}\nD=A\n@R15\nM=D\n@FUNCTION\n0;JMP\n(RET_ADDRESS_FUNCTION{x})\n"
+        return f"({functionName})\n@RET_ADDRESS_FUNCTION_{x}\nD=A\n@R14\nM=D\n@{numLocals}\nD=A\n@R15\nM=D\n@FUNCTION\n0;JMP\n(RET_ADDRESS_FUNCTION_{x})\n"
 
     def writeReturn(self):
         return f"@RETURN\n0;JMP\n"
@@ -74,12 +86,10 @@ class CodeWriter():
         x=str(func_call_counts.setdefault("call",0))
         func_call_counts["call"]+=1
         return f"@{functionName}\nD=A\n@R15\nM=D\n@{numArgs}\nD=A\n@R14\nM=D\n"+\
-            f"@RET_ADDRESS_CALL{x}\nD=A\n@CALL\n0;JMP\n(RET_ADDRESS_CALL{x})\n"
+            f"@RET_ADDRESS_CALL_{x}\nD=A\n@CALL\n0;JMP\n(RET_ADDRESS_CALL_{x})\n"
 
     def writeInIt(self):
-        _return=f"(RETURN)\n@LCL\nD=M\n@R15\nM=D\n"+f"@SP\nA=M-1\nD=M\n@ARG\nA=M\nM=D\n"+f"@ARG\nD=M+1\n@SP\nM=D\n"+\
-                f"@R15\nAM=M-1\nD=M\n@THAT\nM=D\n@R15\nAM=M-1\nD=M\n@THIS\nM=D\n"+\
-                f"@R15\nAM=M-1\nD=M\n@ARG\nM=D\n@R15\nAM=M-1\nD=M\n@LCL\nM=D\n"+f"@R15\nA=M-1\nA=M\n0;JMP\n"
+        _return=f"(RETURN)\n@LCL\nD=M\n@R13\nM=D\n@5\nA=D-A\nD=M\n@R14\nM=D\n@SP\nAM=M-1\nD=M\n@ARG\nA=M\nM=D\n@ARG\nD=M+1\n@SP\nM=D\n@R13\nAM=M-1\nD=M\n@THAT\nM=D\n@R13\nAM=M-1\nD=M\n@THIS\nM=D\n@R13\nAM=M-1\nD=M\n@ARG\nM=D\n@R13\nAM=M-1\nD=M\n@LCL\nM=D\n@R14\nA=M\n0;JMP\n"
         
         call=f"(CALL)\n@SP\nA=M\nM=D\n@SP\nM=M+1\n@LCL\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n@ARG\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"+\
         f"@SP\nD=M\n@R14\nD=D-M\n@5\nD=D-A\n@ARG\nM=D\n@SP\nD=M\n@LCL\nM=D\n@R15\nA=M\n0;JMP\n"
